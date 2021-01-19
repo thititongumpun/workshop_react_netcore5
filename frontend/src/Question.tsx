b18e8d5a577bd45c926ba9e67cabefeb96e09136
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-01-19 17:22:40 +0700

    random btn for stupid people

--- frontend/src/Question.tsx
+++ frontend/src/Question.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { gray2, gray3 } from './Styles';
+import { PrimaryButton } from './Styles';
 
 import React from 'react';
 import { QuestionData } from './data/QuestionsData';
@@ -12,58 +13,64 @@ interface Props {
   showContent?: boolean;
 }
 
-let arr: string[] = [
-  'ไก่ย่าง',
-  'กระเพรา',
-  'ผงกะหรี่',
-  'ทอดกระเทียม',
-  'ขี้เมา',
-  'น้ำมันหอย',
-  'pizza',
-  'kfc',
-];
+export const Question = ({ data, showContent = true }: Props) => {
+  const food: string[] = [
+    'ไก่ย่าง',
+    'กระเพรา',
+    'ผงกะหรี่',
+    'ทอดกระเทียม',
+    'ขี้เมา',
+    'น้ำมันหอย',
+    'pizza',
+    'kfc',
+  ];
+  const [random, setRandom] = React.useState('บุฟเฟ่');
 
-let randomArr: string = arr[Math.floor(Math.random() * arr.length)];
-
-export const Question = ({ data, showContent = true }: Props) => (
-  <div
-    css={css`
-      padding: 10px 0px;
-    `}
-  >
-    <Link
-      css={css`
-        text-decoration: none;
-        color: ${gray2};
-      `}
-      to={`/questions/${data.questionId}`}
-    >
-      {data.title}
-    </Link>
-
-    {showContent && (
-      <div
-        css={css`
-          padding-bottom: 10px;
-          font-size: 15px;
-          color: ${gray2};
-        `}
-      >
-        {data.content.length > 50
-          ? `${data.content.substring(0, 50)}...`
-          : data.content}
-      </div>
-    )}
-    {randomArr}
+  const randomFoods = (e: any) => {
+    const len = food.length;
+    setRandom(food[Math.floor(Math.random() * len)]);
+  };
+  return (
     <div
       css={css`
-        font-size: 12px;
-        font-style: italic;
-        color: ${gray3};
+        padding: 10px 0px;
       `}
     >
-      {`Asked by ${data.userName} on
+      <Link
+        css={css`
+          text-decoration: none;
+          color: ${gray2};
+        `}
+        to={`/questions/${data.questionId}`}
+      >
+        {data.title}
+      </Link>
+
+      {showContent && (
+        <div
+          css={css`
+            padding-bottom: 10px;
+            font-size: 15px;
+            color: ${gray2};
+          `}
+        >
+          {data.content.length > 50
+            ? `${data.content.substring(0, 50)}...`
+            : data.content}
+        </div>
+      )}
+      <h2>{random}</h2>
+      <PrimaryButton onClick={randomFoods}>Random</PrimaryButton>
+      <div
+        css={css`
+          font-size: 12px;
+          font-style: italic;
+          color: ${gray3};
+        `}
+      >
+        {`Asked by ${data.userName} on
         ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}
+      </div>
     </div>
-  </div>
-);
+  );
+};
